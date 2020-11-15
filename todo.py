5a27941c798475fbd7842163c5b2b9bb208f46b8
githsem <53379599+githsem@users.noreply.github.com>
2020-11-16 00:29:09 +0100

    Update todo.py

--- todo.py
+++ todo.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Hallo Welt/Documents/GitHub/Todo_App/todo.db'
 db = SQLAlchemy(app)
-
-
 @app.route("/")
 def index():
     todos = Todo.query.all()
